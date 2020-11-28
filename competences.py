6b8dfe7e309f7d92c4ecdc837ed9f51d573fbbc9
PithonGabriel <74708317+PithonGabriel@users.noreply.github.com>
2020-11-28 19:34:40 +0100

    Add files via upload

---
+++ competences.py
@@ -0,0 +1,23 @@
+import pygame
+
+class Competence():
+    def __init__(self,classe):
+        self.classe=classe
+    
+    def competence1(self):
+        if self.classe=="fighter":
+            print("selectionne la competence 1 fighter")
+        if self.classe=="wizard":
+            print("selectionne la competence 1 sale wizard")
+        if self.classe=="rogue":
+            print("selectionne la competence 1 sale rogue")
+    def competence2(self):
+        print("selectionne la competence 2 sale fou")
+    def competence3(self):
+        print("selectionne la competence 3 sale fou")
+    def competence4(self):
+        print("selectionne la competence 4 sale fou")
+    def competence5(self):
+        print("selectionne la competence 5 sale fou")
+    
+    
---
+++ rogue.py
@@ -0,0 +1,15 @@
+import pygame
+from personnage import *
+
+class Fighter(Perso):
+
+    def __init__(self):
+        super().__init__(classe="rogue")
+        self.velocity=10
+        # self.image = pygame.image.load('projet pygame/assets/Fighter.png')
+
+        # self.trans=pygame.image.load('projet pygame/assets/transformation.png')
+
+
+    def get_class(self):
+        print(self.classe)
