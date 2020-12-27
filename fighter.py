4c30c31a775adfd5907ce8a39f71db5db9cd2dbe
Gabriel <Gabriel.pithon@outlook.fr>
2020-12-27 22:41:50 +0100

    inter2

--- fighter.py
+++ fighter.py
@@ -7,11 +7,37 @@ class Fighter(Perso):
         "proficent avec toutes les armes"
         super().__init__(classe="fighter")
         self.hit_dice=10
-
+        "si SW (Second Wind) est a true il est utilisable"
+        self.SW=False
 
     def levelupchange(self):
         super().levelupchange() 
         self.attack=self.level
+        if self.level==1:
+            self.SW=True
+
+    def rest(self):
+        super().rest()
+        self.SW=True
+
+    ### Actions ###
+    def secondWind(self):
+        if self.SW==True:
+            hp_bonus=self.action.dice(10)
+            if
+
+    
+    def extra_attack(self):
+        if self.masterAction>0 and self.bonusAction>0:
+            self.Action=2
+            self.masterAction-=1
+            self.bonusAction-=1
+        else:
+            running=True
+            while running:
+                running=board_error("no bonus action or Master Action left")
+    
+    ### Fin des actions ###
 
 """to do:
     def action_surge(self): |lvl1
